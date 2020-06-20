df0f61a30a2003df364cdf6981ef6b3cd5349605
Juanma Vega <juanmavega83@gmail.com>
2020-06-20 02:02:58 +0200

    First commit

---
+++ web/src/Controller/AdminController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+ * @Route("/admin")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_main_page")
+     */
+    public function index()
+    {
+        return "polla";
+    }
+
+
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function categories()
+    {
+        return $this->render('admin/categories.html.twig');
+    }
+
+    /**
+     * @Route("/edit-category", name="edit_category")
+     */
+    public function editCategory()
+    {
+        return $this->render('admin/edit_category.html.twig');
+    }
+
+    /**
+     * @Route("/videos", name="videos")
+     */
+    public function videos()
+    {
+        return $this->render('admin/videos.html.twig');
+    }
+
+    /**
+     * @Route("/upload-video", name="upload_video")
+     */
+    public function uploadVideo()
+    {
+        return $this->render('admin/upload_video.html.twig');
+    }
+
+    /**
+     * @Route("/users", name="users")
+     */
+    public function users()
+    {
+        return $this->render('admin/users.html.twig');
+    }
+
+        /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
+
